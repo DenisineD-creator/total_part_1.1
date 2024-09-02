1791d8686a26a31e0b98b1b925a4a96501ced161
DenisineD-creator <dburobin1@mail.ru>
2024-09-02 17:42:04 +0300

    Add files via upload

---
+++ total_part_1.1.py
@@ -0,0 +1,13 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny','Bilbo','Steven','Khendrik','Aaron'}
+student_sort = sorted(students)
+grades_m = sum(grades[0])/len(grades[0]),
+grades_m1 = sum(grades[1])/len(grades[1]),
+grades_m2 = sum(grades[2])/len(grades[2]),
+grades_m3 = sum(grades[3])/len(grades[3]),
+grades_m4 = sum(grades[4])/len(grades[4])
+print(grades_m,grades_m1,grades_m2,grades_m3,grades_m4)
+#student_sort = sorted(students)
+print(student_sort)
+dict1 = (zip(student_sort,grades_m))
+print(dict1)
